8082399d0b2b26b749c4624305ed4dd7dbd10975
HyperGamteng <66789701+nopal0871@users.noreply.github.com>
2022-12-21 16:54:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281390658325'],
-  ['6282256115584'],
-  ['6281319944687', 'BG', true]
+  ['6285781628368', 'oksi', true],
+  ['6281275327160' 'yud yud', true],
+  ['6285781628368', 'oxxy', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.prems = ['085781628368', '621275325160'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -30,8 +30,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot'
+global.packname = 'wa.me/6285781628368'
+global.author = 'MOxxy Bot'
 
 global.multiplier = 69 // The higher, The harder levelup
 
