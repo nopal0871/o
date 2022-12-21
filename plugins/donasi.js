b00f4639a9bc8ef37de6ed53878186fb6c8425b3
HyperGamteng <66789701+nopal0871@users.noreply.github.com>
2022-12-21 16:56:58 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,10 @@
 let handler =  m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [085713964963]
-│ • Telkomsel [081319944687]
+│ • Indosat Ooredoo [085781628368]
 ╰────
 
 ╭─「 Donasi • Non Pulsa 」
-│ • https://saweria.co/BochilGaming
-│ • Gopay [085713964963]
+│ • Gopay [085781628368]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
